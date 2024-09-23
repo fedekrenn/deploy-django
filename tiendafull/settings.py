c52b6ee2734f03e5bed6cf5c57646aaa6256dce7
Fede Krenn <fedekrenn@gmail.com>
2024-09-22 22:35:37 -0300

    Test

--- tiendafull/settings.py
+++ tiendafull/settings.py
@@ -51,6 +51,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.middleware.common.CommonMiddleware",
     "django.middleware.csrf.CsrfViewMiddleware",
@@ -86,7 +87,7 @@ WSGI_APPLICATION = "tiendafull.wsgi.application"
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
-    "default": dj_database_url.config(
+    "default": dj_database_url(
         default='postgresql://fkrenn:31CR9kCLpC6v3YaE227AgxvNxqkKd8U8@dpg-crobpb88fa8c738pv2e0-a.oregon-postgres.render.com/tiendafull',
         conn_max_age=600
     )
@@ -128,6 +129,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = "static/"
+
+if not DEBUG:
+    STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+    STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
